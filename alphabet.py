c515cf3c8a102881157db0e0c476c19f4342f138
Sandhiya-B <52743904+Sandhiya-B@users.noreply.github.com>
2019-07-10 17:18:30 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+ch = input()
+if ch in ('A','E','I','O','U','a','e','i','o','u','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Alphabet")
+else:
+    print("No")
