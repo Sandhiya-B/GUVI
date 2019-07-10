518cadb91ecddac981ad33143da776647825c834
Sandhiya-B <52743904+Sandhiya-B@users.noreply.github.com>
2019-07-10 17:15:03 +0530

    Create vowelconso.py

---
+++ vowelconso.py
@@ -0,0 +1,7 @@
+ch = input()
+if ch in ('A','E','I','O','U','a','e','i','o','u'):
+    print("Vowel")
+elif ch in ('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonant")
+else:
+    print("Invalid")
